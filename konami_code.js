cb49836d5f35a9655dda6b94788217d0300bfc80
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-12 00:33:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,43 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
-}
+const getElement = document.getElementsByTagName('body')
+
+let index = 0
+
+function init(e){
+
+  if (code[index] === e.which || code[index] === e.detail){
+    index++
+
+  if (index === code.length){
+    alert("Yay!")
+    index = 0
+  } else {
+    index = 0
+  }}}
+
+document.addEventListener("keydown", init)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//tasks//
+//create event listener for keyboard//
+//create a function that listens for a specific event //
+    //if that occurs, enter a function that listens for a second event//
+      //and so on//
+        //create a function so that if code is completed, an alert with message gets printed//
+//if code is not entered properly, listener events always restart//
