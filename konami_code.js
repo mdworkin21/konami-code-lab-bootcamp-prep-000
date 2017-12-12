c78108e2d243994b0914c34a0688dc134d0902d3
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-12 00:53:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 const body = document.getElementsByTagName('body')
 
-document.body.addEventListener("keydown", init)
 
 let index = 0
 
@@ -23,6 +22,8 @@ function init(){
   }
 
 }
+
+document.body.addEventListener("keydown", init)
 
 
 
