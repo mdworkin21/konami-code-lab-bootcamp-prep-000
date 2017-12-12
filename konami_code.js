18105abf41d34b95ff64816bd75bd98aa97d8a70
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-12 00:59:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,14 +2,12 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
 
-
-
 function init(){
   const body = document.getElementsByTagName('body')
 
 
   let index = 0
-  let key = document.body.addEventListener("keypress", function(e){
+  let key = document.body.addEventListener("keyboardEvent", function(e){
 
   if (e.which === code[index]){
     while (key === code[index]){
