9832fad946ae29978b2eb59080719e8e34182ff1
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-12 00:58:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,13 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
-const body = document.getElementsByTagName('body')
 
-
-let index = 0
 
 function init(){
+  const body = document.getElementsByTagName('body')
+
+
+  let index = 0
   let key = document.body.addEventListener("keydown", function(e){
 
   if (e.which === code[index]){
